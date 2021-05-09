927b728d872c4d419eee07cc0ccde12322667085
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-09 11:07:09 +0100

    added field generator

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+
+
 class Field {
-    constructor(field){
-        this.field = field;
+    constructor(field) {
+        this.field = field || [];
     }
     print() {
         for (let row of this.field){
-            console.log(row.join(' '));
+          console.log(row.join(' '));
         }
       }
+    generateField(height, width) {
+        let randomNum = Math.round(Math.random() * 10) + 2;
+        for (let row = 0; row < randomNum ; row ++){
+            let row = [];
+            for (let column = 0; column < randomNum; column ++){
+                row.push(fieldCharacter);
+            }
+            this.field.push(row);
+        }
+    }
+
 }
 
-const myField = new Field([
-      ['*', '░', 'O'],
-      ['░', 'O', '░'],
-      ['░', '^', '░'],
-    ]);
+const myField = new Field();
+
+myField.generateField();
 
 myField.print();
-const name = prompt('What is your name ');
-console.log(name)
